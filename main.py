45de975bcd6373a04a27178623c26a0e1e1d45e4
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-28 13:43:24 +0000

    Exercicio resolvido e testado

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-n_termo = int(input('Digite o termo que deseja: '))
-if n_termo < 0:
-    raise ValueError("n tem que ser maior do que zero")
-    
 def fibonacci(termo):
-    num = 0
+    if termo < 0:
+        raise ValueError("n tem que ser maior do que zero")
+    elif termo == 0:
+        #print(termo)
+        return 0
+    num = 1
     num_Prev = 0
     num2_Prev = 1
-    for id in range(0,termo):
+    for id in range(1,termo):
         if num==num_Prev:
             num += num2_Prev
         elif num > num_Prev:
             num2_Prev = num
             num += num_Prev
             num_Prev = num
-    return print(num_Prev)
+    #print(num)
+    return num
 
-fibonacci(n_termo)
+if __name__ == "__main__":
+    n_termo = int(input('Digite o termo que deseja: '))
+    fibonacci(n_termo)
